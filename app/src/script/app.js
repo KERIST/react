b728acdaa83dbc29032511849146c8487c07a6a2
ICX\aliaksandr.nikifarau <alexandernanotexno@gmail.com>
2017-11-23 11:16:29 +0300

    changed

--- app/src/script/app.js
+++ app/src/script/app.js
@@ -1,3 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
 require('../style/styles.scss');
+
+class Test extends React.Component {
+    render(){
+        return <div>Hello</div>;
+    }
+}
+
+render(<Test/>, document.querySelector('#app'));
 
 console.log('helloq');
